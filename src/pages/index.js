98471cea59a66ef0e8eb15eb9fb6b7d0170d5eea
IORP™ <indianoceanroleplay@gmail.com>
2020-10-10 14:19:24 +0530

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -56,8 +56,7 @@ function Home() {
   const {siteConfig = {}} = context;
   return (
     <Layout
-      title={`${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}>
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
